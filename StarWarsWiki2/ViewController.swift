d5abecbc3c053ce5231a46c5a5e4a8ac05a2020c
Ivan Grishin <ktezik@yandex.ru>
2023-08-07 13:48:23 +0300

    Initial Commit

---
+++ StarWarsWiki2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsWiki2
+//
+//  Created by Иван Гришин on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
